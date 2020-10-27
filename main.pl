df2187948195cdcd73e76970ca1225d2bc86c1a0
Alyssa Nolan <canalyssa@outlook.com>
2020-10-27 09:09:41 +0530

    Add detector

--- main.pl
+++ main.pl
@@ -9,3 +9,16 @@ if($n_args != 0){
     warn "Invaild number of arguments.\n";
     exit 1;
 }
+
+open(FH, '<', @ARGV) or die $!;
+
+while(<FH>){
+    if(index($_, "\t") != -1){
+      print "Oopsie, got tab :(\n";
+      exit 1;
+    }
+}
+
+close(FH);
+
+print "All clear, no tabs :)\n";
